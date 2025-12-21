0e0b47e9174199d204b91d61edead6ca8a0cc479
lxd <648612680@qq.com>
2025-12-21 14:20:02 +0800

    feat(run-in-every-page): 添加在每个页面运行的 Chrome 扩展功能
    新增了一个 manifest.json 配置文件，定义了扩展的基本信息、图标及内容脚本。
    添加了 content.js 脚本，用于计算文章阅读时间并在页面中显示。
    该脚本通过监听 DOM 变化，支持 SPA 页面的内容更新。
    同时添加了所需的多尺寸图标资源。
    
    此外，更新了 popup.js 中的注释，明确指出控制台输出的位置。

--- hello/popup.js
+++ hello/popup.js
@@ -1,1 +1,2 @@
 console.log('this is popup.js');
+console.log('这个console将出现在google extension的popup页面中，而不是当前浏览的网页中');
---
+++ run-in-every-page/scripts/content.js
@@ -0,0 +1,51 @@
+console.log('这个console出现在浏览器中，而不是出现在google extension的审查元素中');
+
+function renderReadingTime (article) {
+  console.log(999999);
+  
+  // If we weren't provided an article, we don't need to render anything.
+  if (!article) {
+    return;
+  }
+
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
+
+renderReadingTime(document.querySelector("article"));
+
+// Create a mutation observer to watch for new articles being added to the DOM.
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    // If a new article was added.
+    for (const node of mutation.addedNodes) {
+      if (node instanceof Element && node.tagName === 'ARTICLE') {
+        // Render the reading time for this particular article.
+        renderReadingTime(node);
+      }
+    }
+  }
+});
+
+// https://developer.chrome.com/ is a SPA (Single Page Application) so can
+// update the address bar and render new content without reloading. Our content
+// script won't be reinjected when this happens, so we need to watch for
+// changes to the content.
+observer.observe(document.querySelector('devsite-content'), {
+  childList: true
+});
